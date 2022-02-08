1d41cc0f51cd340c94eeb41a971c062e3e687396
shikharpathak <shikharpathak96@gmail.com>
2022-02-08 11:36:45 +0530

    winning logic done

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
-const getHash = require("./game-engine/mapper");
+// const getHash = require("./game-engine/mapper");
 
 var WebSocketServer = require("ws").Server,
   wss = new WebSocketServer({ port: 8080 });
+const hashMap = new Map();
+
+hashMap.set("1", 11);
+hashMap.set("2", 13);
+hashMap.set("3", 17);
+hashMap.set("4", 19);
+hashMap.set("5", 23);
+hashMap.set("6", 29);
+hashMap.set("7", 31);
+hashMap.set("8", 37);
+hashMap.set("9", 41);
+
+function getHash(key) {
+  return hashMap.get(key.toString());
+}
 
 let gridFinal = ["_", "_", "_", "_", "_", "_", "_", "_", "_"];
 let numberOfVisitors = 0;
 let value = 100;
 positions = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8]);
 let symbol = null;
+const winX = "X has won";
+const winO = "O has won";
 wss.on("connection", function (ws, req) {
   const assignment = ["X", "O"];
   symbol = numberOfVisitors < 2 ? assignment[numberOfVisitors] : "#";
@@ -25,16 +42,17 @@ wss.on("connection", function (ws, req) {
       const symbolOfPlayer = splitMessage[0];
 
       value = Number(splitMessage[1]) - 1;
-
       if (
         symbolOfPlayer == "X" ||
         (symbolOfPlayer == "O" && positions.has(value))
       ) {
         positions.delete(value);
+        console.log(positions);
 
         const gameState = winningLogic(symbolOfPlayer, value + 1);
-
-        if (gameState == "O has won " || gameState == "X has won") {
+        console.log(gameState);
+        if (gameState == winO || gameState == winX) {
+          console.log("END GAME");
           ws.close(1000, gameState);
           wss.on("close", function close() {
             ws.send(gameState);
@@ -54,8 +72,8 @@ wss.on("connection", function (ws, req) {
     });
   }
 });
-const positionsOf_X = [];
-const positionsOf_O = [];
+let positionsOf_X = [];
+let positionsOf_O = [];
 function winningLogic(turn, position) {
   const winningSum = [41, 71, 109, 75, 71];
 
@@ -63,15 +81,32 @@ function winningLogic(turn, position) {
 
   if (turn == "X") {
     positionsOf_X.push(hash);
+    positionsOf_X = positionsOf_X.filter((element) => {
+      return element !== undefined;
+    });
+
+    console.log(positionsOf_X);
     const currentSum = positionsOf_X.reduce(
-      (previous, current) => previous + current
+      (previous, current) => previous + current,
+      0
     );
+    console.log("current", currentSum);
     const found = winningSum.find((sum) => sum === currentSum);
-    return found == currentSum ? "X has won " : " Continue";
+    console.log(found);
+    if (found >= 41 && found <= 110)
+      return found == currentSum ? winX : " Continue";
   }
 
   positionsOf_O.push(hash);
-  currentSum = positionsOf_O.reduce((previous, current) => previous + current);
+  positionsOf_O = positionsOf_O.filter((element) => {
+    return element !== undefined;
+  });
+  currentSum = positionsOf_O.reduce(
+    (previous, current) => previous + current,
+    0
+  );
+
   const found = winningSum.find((sum) => sum === currentSum);
-  return found == currentSum ? "O has won " : " Continue";
+  if (found >= 41 && found <= 110)
+    return found == currentSum ? winO : " Continue";
 }
---
+++ src/interfaces/playerResponse.interface.ts
@@ -0,0 +1,3 @@
+export interface playerResponses {
+  Move: string;
+}
